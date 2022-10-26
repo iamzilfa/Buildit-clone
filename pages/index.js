774f84546f4b52e12c171d1e606b3adb859058b4
iamzilfa <zilcyam@gmail.com>
2022-10-26 12:34:42 +0200

    all

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import Head from "next/head";
+import Image from "next/image";
+import Footer from "../components/Footer";
+
+export default function Home() {
+  return (
+    <div>
+      <div className="">
+        <h1>hey</h1>
+      </div>
+
+      <Footer />
+    </div>
+  );
+}
